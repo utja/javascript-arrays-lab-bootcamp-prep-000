c3e5a13013e66484513bfe9773d8fd7217c51522
ahn.jon.s@gmail.com Ahn <ahn.jon.s@gmail.com>
2018-04-25 00:20:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const app = "I don't do much."
 function destructivelyAppendKitten(name) {
   return kittens.push(name)
 }
+
+function desctruvticelyPrependKitten(name) {
+  return kittens.pop(name)
+}
