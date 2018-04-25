ef37bd03199611ae918dc4ace3f60e49183ff42b
ahn.jon.s@gmail.com Ahn <ahn.jon.s@gmail.com>
2018-04-25 00:25:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ function destructivelyAppendKitten(name) {
 function destructivelyPrependKitten(name) {
   return kittens.unshift(name)
 }
+
+function destructivelyRemoveLastKitten(name) {
+  return kittens.pop(name)
+}
